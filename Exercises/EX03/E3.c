f3e1484f8d89ef338a9b3b6bb9b463973767f853
Lef Filippakis <lefphilip@live.com>
2017-11-25 19:26:53 +0100

    EX03 Task 3 is giving bullshit results, Task 4 half done

--- Exercises/EX03/E3.c
+++ Exercises/EX03/E3.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 #include <gsl/gsl_randist.h>
 #include "helper_funcs.h"
 #define PI 3.141592653589
@@ -19,13 +20,14 @@ void one_dim_integral(){
 
   // Initialize vars
   int x, t, N;
-  double error, sum, avg, var_avg, square_sum;
+  double error, sum, avg, var_avg, square_sum, new;
+  double x_var[4][10000];
   // random number generator
   gsl_rng *my_rng = initialize_rng();
 
   // Print expected value to compare
   printf("*******************************************\n");
-  printf("************UNIFORM DISTRIBUTION***********\n");
+  printf("*******************TASK 1******************\n");
   printf("*******************************************\n");
   printf("The expected value of the integral is approx: %.8F\n", (double) 1/6);
   printf("*******************************************\n");
@@ -34,8 +36,6 @@ void one_dim_integral(){
   for(x = 0; x < 4; x++){
     
     N = pow(10,x+1);
-    // Reset according to new length N
-    double x_var[N];
     sum = 0;
     square_sum = 0;
 
@@ -43,14 +43,15 @@ void one_dim_integral(){
     for(t = 0; t < N; t++){
 
       // Get a random sample
-      x_var[t] = gsl_rng_uniform(my_rng);
+      x_var[x][t] = gsl_rng_uniform(my_rng);
 
       
       // Calculate the big sum
-      sum += x_var[t] * (1 - x_var[t]);
+      new = x_var[x][t] * (1 - x_var[x][t]);
+      sum += new;
 
       // Calculate the variance
-      square_sum += pow(x_var[t] * (1 - x_var[t]),2);
+      square_sum += pow(new,2);
 
     }
 
@@ -67,6 +68,17 @@ void one_dim_integral(){
 
   }
   printf("*******************************************\n");
+
+  // Save results in file:
+  FILE *out_file;
+  out_file = fopen("task1.data", "w");
+
+  for(x = 0; x < 10000; x++){
+    fprintf(out_file, "%F \t %F \t %F \t %F \n", x_var[0][x], x_var[1][x], x_var[2][x], x_var[3][x]);
+  }
+
+  fclose(out_file);
+
 }
 
 // Task 2
@@ -75,14 +87,14 @@ void sine_integral(){
 
   // Initialize vars
   int x, t, N;
-  double r, error, sum, avg, var_avg, square_sum;
+  double r, error, sum, avg, var_avg, square_sum, new;
   // Set x array size to max N
   double x_var[4][10000];
   // random number generator
   gsl_rng *my_rng = initialize_rng();
 
   // Print expected value to compare
-  printf("*************SINE DISTRIBUTION*************\n");
+  printf("*******************TASK 2******************\n");
   printf("*******************************************\n");
   printf("The expected value of the integral is approx: %.8F\n", (double) 1/6);
   printf("*******************************************\n");
@@ -101,17 +113,14 @@ void sine_integral(){
       r = gsl_rng_uniform(my_rng);
 
       // Push the random sample through the sine distribution
-      x_var[x][t] = acos(1 - 2*r) / PI;
+      x_var[x][t] = asin(r) / PI;
       
       // Calculate the big sum
-      sum += x_var[x][t] * (1 - x_var[x][t]) * 2 / sin(PI * x_var[x][t]) / PI;
+      new = x_var[x][t] * (1 - x_var[x][t]);
+      sum += new;
 
       // Calculate the variance
-      square_sum += x_var[x][t] * (1 - x_var[x][t]) * 2 \
-                    / sin(PI * x_var[x][t]) * x_var[x][t] * (1 - x_var[x][t]) * 2 \
-                    / sin(PI * x_var[x][t]) \
-                    / PI \
-                    / PI;
+      square_sum += pow(new,2);
 
     }
 
@@ -142,62 +151,78 @@ void sine_integral(){
 
 void metropolis(){
   // Initialize vars
-  int N = 10000, t, x;
-  double p_max = 0.01;
-  double x_var[N], p_var, sum, square_sum, trial, r;
-  double error, avg, var_avg;
-  double delta = 0.55;
+  int N = 10000, t=0, x;
+  double p_m = 0.5; // Initialize probability of state null to 0.5
+  double x_var[N], y_var[N], z_var[N]; 
+  double p_n[N], sum, square_sum = 0, trial, r;
+  double error, avg, var_avg, new;
+  double delta = -0.9;
   int total = 0;
 
   // random number generator
   gsl_rng *my_rng = initialize_rng();
 
-  printf("************METROPOLS INTEGRAL*************\n");
-  printf("*******************************************\n");
-  printf("The expected value of the integral is approx: %.8F\n", (double) 1/6);
+  printf("*******************TASK 3******************\n");
   printf("*******************************************\n");
 
   // Prepare variables before looping
-  x_var[0] = 0.5;
-  p_var = 2 / sin(PI * x_var[0]) / PI;
+  x_var[0] = 0;
+  y_var[0] = 0;
+  z_var[0] = 0;
+  p_n[0] = sqrt(-x_var[t] * x_var[t] - log(pow(PI,-3/2) * y_var[t]) \
+          - z_var[t] * z_var[t]);
+  sum = 0;
+  square_sum = 0;
 
   for(t = 1; t < N; t++){
     
     // Let the trials begin!
+
+    // Get random
     do {
+      r = gsl_rng_uniform(my_rng);
+    } while (r > 1.0); // Just to be safe
 
-      // Get random
-      do {
-        r = gsl_rng_uniform(my_rng);
-      } while (r > 1.0);
+    // Calculate next value of x for the trial
+    x_var[t] =  x_var[t - 1] + delta * (r - 0.5);
+    y_var[t] =  y_var[t - 1] + delta * (r - 0.5);
+    z_var[t] =  z_var[t - 1] + delta * (r - 0.5);
 
-      // Calculate next value of x for the trial
-      x_var[t] =  x_var[t - 1] + delta * (r - 0.5);
+    // Transform through the inverse of the weight function!
+    p_n[t] = sqrt( - pow(x_var[t],2) - log(pow(PI,-3/2) * y_var[t]) \
+          - pow(z_var[t],2));
+    // Ratio of current state prob over previous state prob
+    trial = p_n[t] / p_m;
 
-      // Find the probability
-      p_var = 2 / sin(PI * x_var[t]) / PI;
+    // Get new random
+    do {
+      r = gsl_rng_uniform(my_rng);
+    } while (r > 1.0); // Just to be safe
 
-      trial = p_var / p_max;
-
-      // Get random
-      do {
-        r = gsl_rng_uniform(my_rng);
-      } while (r > 1.0);
-
-      total++;
 
     // Test for the Rejection Method
-    } while (trial < r);
+    if(trial < r){
+      // Not accepted, keep previous value
+      x_var[t] = x_var[t - 1];
+      y_var[t] = y_var[t - 1];
+      z_var[t] = z_var[t - 1];
+      total++;
+    } else {
+      // Accepted
+      // Update the probability of the previous state
+      // to the one of the current state
+      p_m = p_n[t];
+    }
     
     // Calculate the big sum
-    sum += x_var[t] * (1 - x_var[t]) * 2 / sin(PI * x_var[t]) / PI;
+    new = pow(PI,-3/2) * pow(x_var[t],2) + pow(x_var[t],2) * pow(y_var[t],2) \
+          + pow(x_var[t],2) * pow(y_var[t],2) * pow(z_var[t],2) \
+          * exp( - (pow(x_var[t],2) + pow(y_var[t],2) + pow(z_var[t],2)) );
+
+    sum += new;
 
     // Calculate the variance
-    square_sum += x_var[t] * (1 - x_var[t]) * 2 \
-                  / sin(PI * x_var[t]) * x_var[t] * (1 - x_var[t]) * 2 \
-                  / sin(PI * x_var[t]) \
-                  / PI \
-                  / PI;
+    square_sum += pow(new,2);
 
   }
 
@@ -212,18 +237,83 @@ void metropolis(){
 
   // Results!!
   printf("N = %i | Integral value = %.4F | Error margin = %.4F \n", N, avg, error );
-  printf("Percentage of approved trials = %.1f\n", 100*(N/total) );
-  printf("%i\n", total);
-
+  printf("*******************************************\n");
+  printf("Percentage of approved trials = %.1f\n", (double) 100*(((double) N - (double) total) / (double) N) );
+  printf("*******************************************\n");
+  
   // Save results in file:
   FILE *out_file;
   out_file = fopen("task3.data", "w");
 
   for(x = 0; x < N; x++){
-    fprintf(out_file, "%F \n", x_var[x]);
+    fprintf(out_file, "%F \t %F \t %F \t %F\n", x_var[x], y_var[x], z_var[x], p_n[x]);
   }
 
   fclose(out_file);
+}
+
+// Task 4a
+void correlation_function(double *array, int array_length){
+  printf("******************TASK 4a******************\n");
+  printf("*******************************************\n");
+
+  // Initialize vars
+  int steps = 300; // Steps for the correlation function
+  int decay_time;
+  double mean_fi = 0, square_mean_fi = 0;
+  double correlation_func[steps], fik[steps];
+  double stat_ineff = 0, relaxation_time = 0, total_error;
+
+  // Array of zeros
+  for (int k = 0; k < steps; ++k){
+    fik[k] = 0.0;
+  }
+
+  // Calculate <f_i> and <f_i>^2
+  for (int i = 0; i < array_length - steps; ++i){
+    mean_fi += array[i] / (array_length - steps);
+    square_mean_fi += pow(array[i],2) / (array_length - steps);
+  }
+
+  // Calculate <f_(i+k)*f_i> 
+  for (int i = 0; i < array_length - steps; ++i){
+    for (int k = 0; k < steps; ++k){
+      fik[k] += (array[i + k] * array[i]) / (array_length - steps);
+    } 
+  }
+
+  // Calculate correlation function
+  for (int k = 0; k < steps; ++k){
+    correlation_func[k] = ( fik[k] - pow(mean_fi,2) ) \
+                          / ( square_mean_fi - pow(mean_fi,2)); 
+  }
+
+  // Calculate the time when the function will have decayed by approximately 10%
+  // aka statistical inefficiency
+  decay_time = 0;
+  while(correlation_func[decay_time] >= exp(-2)){
+    relaxation_time += correlation_func[decay_time];
+    decay_time++;
+  }
+
+  stat_ineff = relaxation_time * 2;
+
+  printf("Function reaches 10%% of it's initial value %i times!\n", decay_time);
+  total_error = sqrt((square_mean_fi - mean_fi*mean_fi)/steps*stat_ineff);
+  printf("Statistical inefficiency: %F \n", stat_ineff);
+  printf("Result: %.8e | Error margin: %.10e \n", mean_fi, total_error);
+
+  printf("*******************************************\n");
+}
+
+// Task 4b
+void block_averaging(double *array, int array_length){
+  printf("******************TASK 4b******************\n");
+  printf("*******************************************\n");
+
+
+
+  printf("*******************************************\n");
 }
 
 int main()
@@ -233,11 +323,12 @@ int main()
   
   nbr_of_lines = 1e6; /* The number of lines in MC.txt. */
   double *data = malloc((nbr_of_lines) * sizeof (double));
+  double temp;
   
   /* Read data from file. */
   in_file = fopen("MC.txt","r");
   for (i=0; i<nbr_of_lines; i++) {
-    fscanf(in_file,"%lf",&data[i]);
+    temp = fscanf(in_file,"%lf",&data[i]);
   }
   fclose(in_file);
 
@@ -250,6 +341,12 @@ int main()
 
   // Run Task 3
   metropolis();
+
+  // Run Task 4a
+  correlation_function(data, nbr_of_lines);
+
+  // Run Task 4b
+  block_averaging(data, nbr_of_lines);
 }
 
 
---
+++ Exercises/EX03/E3_plots.m
@@ -0,0 +1,128 @@
+%% % Task 1 plots
+
+clc; clf; clear all;
+nbins = 25;
+
+% load data file
+distData = importdata('task1.data');
+
+%plot
+figure(1);
+subplot(2,2,1)
+histogram(distData(1:10,1), nbins,'Normalization', 'probability');
+title('N = 10');
+%xlim([0 1]);
+
+subplot(2,2,2)
+histogram(distData(1:100,2),nbins,'Normalization', 'probability');
+title('N = 100');
+%xlim([0 1]);
+
+subplot(2,2,3)
+histogram(distData(1:1000,3),nbins,'Normalization', 'probability');
+title('N = 1000');
+%xlim([0 1]);
+
+subplot(2,2,4)
+histogram(distData(1:10000,4),nbins,'Normalization', 'probability');
+title('N = 10000');
+xlim([0 1]);
+
+
+%% Task 2 plots
+
+clc; clf; clear all;
+nbins = 25;
+
+% energies
+% load the data file
+distData = importdata('task2.data');
+
+%plot
+figure(1);
+subplot(2,2,1)
+histogram(distData(1:10,1), nbins,'Normalization', 'probability');
+title('N = 10');
+xlim([0 1]);
+
+subplot(2,2,2)
+histogram(distData(1:100,2),nbins,'Normalization', 'probability');
+title('N = 100');
+xlim([0 1]);
+
+subplot(2,2,3)
+histogram(distData(1:1000,3),nbins,'Normalization', 'probability');
+title('N = 1000');
+xlim([0 1]);
+
+subplot(2,2,4)
+histogram(distData(1:10000,4),nbins,'Normalization', 'probability');
+title('N = 10000');
+xlim([0 1]);
+
+
+%% Task 3
+
+clc; clf; clear all;
+nbins = 25;
+
+data = importdata('task3.data');
+
+% plot
+figure(1);
+subplot(3,3,1);
+histogram(data(:,1), nbins, 'Normalization','probability');
+title('Distribution of x');
+xlabel('Probability');
+ylabel('x');
+
+subplot(3,3,2);
+histogram(data(:,2), nbins, 'Normalization','probability');
+title('Distribution of y');
+xlabel('Probability');
+ylabel('y');
+
+subplot(3,3,3);
+histogram(data(:,3), nbins, 'Normalization','probability');
+title('Distribution of z');
+xlabel('Probability');
+ylabel('z');
+
+subplot(3,3,4);
+plot(1:10000,data(:,1));
+title('Values of x over time');
+xlabel('N');
+ylabel('x');
+
+subplot(3,3,5);
+plot(1:10000,data(:,2));
+title('Values of y over time');
+xlabel('N');
+ylabel('y');
+
+subplot(3,3,6);
+plot(1:10000,data(:,3));
+title('Values of z over time');
+xlabel('N');
+ylabel('z');
+
+subplot(3,3,7:9);
+histogram(data(:,4), nbins, 'Normalization','probability');
+title('Probability distribution of p_n');
+xlabel('p_n');
+ylabel('Probability');
+
+%% Task 4
+clc; clf; clear all;
+
+data = importdata('MC.txt');
+numlags = 1000;
+
+[corr,lags] = xcorr(data);
+
+corr = gather(corr);
+
+corr = autocorr(data, numlags);
+
+subplot(1,1,1)
+plot(0:numlags,corr);
